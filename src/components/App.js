18209d4166b6e752624ff06391e44a9400a2447f
ElvyKamande <elvy.waithera@student.moringaschool.com>
2025-04-11 11:03:05 +0300

    Well imported and exported

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import {username,city,image} from "../data/user";
+import Home from "./Home";
+import About from "./About";
+import NavBar from "./NavBar";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import home from "./components/Home";   
+import about from "./components/About";
+import NavBar from "./components/NavBar";
 
 ReactDOM.render(<App />, document.getElementById("root"));
